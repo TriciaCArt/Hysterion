54b6d8b08f1440906ffd98cfbd030a69d41f3dbf
TriciaCArt <illiannamama@gmail.com>
2022-04-12 17:06:13 -0400

    Getting it to work

--- js/site.js
+++ js/site.js
@@ -1,27 +1,44 @@
 // Step One - get -controller accept requrests
 function getMessage() {
     let msg = document.getElementById("message").value;
-    displayMessage(msg);
+
+    if (msg == '') {
+        alert('YOU GOTTA PUT A WORD IN! Empty space is not a palidrome!')
+    } else {
+        msg = msg.toLowerCase();
+        msg = msg.replace(/[^A-Z0-9]/ig, "");
+        let what = reverseMessage(msg);
+        let letMeSee = msg;
+
+        if (what == letMeSee) {
+            displayMessage('Yes! This is a palindrome!');
+
+        } else {
+            displayMessage('Nope. Try again.');
+
+        }
+
+    }
+
+}
+
+function reverseMessage(msg) {
+    let revMessage = '';
+
+    for (let i = msg.length - 1; i >= 0; i--) {
+
+        let char = msg[i];
+        revMessage += char;
+
+    }
+
+    return revMessage
 }
 
 // final step - display those values
+
 function displayMessage(message) {
-    // <li class="list-group-item">A List Item</li>
-
-    // let item = `<li class="list-group-item">${message}</li>`;
-
-    // Frist get the ol element from the page
-    element = document.getElementById("results");
-
-    // Next create a new li element
-    let item = document.createElement("li");
-    // add classes to the li element
-    item.classList.add("list-group-item");
-    // set the message for the li element
-    item.innerHTML = message;
-
-    // add the new item to the list
-    element.appendChild(item);
-
+    let yay = document.getElementById('results');
+    yay.innerHTML = message;
 
 }
